12b97a5aa32ec8f2d746f6fba65171d2ffeb86c8
joe0731 <222375587+joe0731@users.noreply.github.com>
2026-01-14 17:56:01 +0800

    update

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if readme_path.exists():
 
 setup(
     name="gitlab-pr-analyzer",
-    version="0.9.3",
+    version="0.9.4",
     author="GitLab PR Analyzer Team",
     description="Intelligent tool for collecting, analyzing, and summarizing GitLab Merge Requests and commits",
     long_description=long_description,
